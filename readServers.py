c9b67f00eab1f9a32c0a2979fecb9df6489167e3
CharlesM <cmanker@email.unc.edu>
2013-10-10 16:27:11 -0400

    Added the readServer method to make a mapping between IPs and departments

---
+++ readServers.py
@@ -0,0 +1,45 @@
+def readServers(file1, file2):
+    ips = open(file1,'r'); departs = open(file2,'r')
+    serversDict = dict()    
+    departs.readline() # first line is junk, get rid of it
+    x = departs.readline()        
+    while x != '':
+        val = x.partition(',')[0]
+        key = (x.partition(',')[2]).partition('\n')[0]
+        serversDict[key] = val,
+        x = departs.readline()
+    ips.readline() # first line is junk, get rid of it
+    w = ips.readline() 
+    while w != '':
+        key = w.partition(',')[0]
+        val = ((w.partition(',')[2]).partition(',')[2]).partition('\n')[0]
+        if val in serversDict:
+            department = serversDict[val]
+            if key in serversDict:
+                oldVal = serversDict[key]
+		serversDict[key] = oldVal + department
+            else:
+                serversDict[key] = department
+        w = ips.readline()
+    return serversDict
+
+# Notes on usage:
+
+# Call the method readServers() with the two files as parameters
+# HOSTS.csv and ag_list.csv, in that order.
+
+# The method returns a dictionary.
+# The dictionary has a key (IP Address) and value (Department names).
+
+# The dictionary will return a tuple of a variable number of elements. 
+# Iterate through it to get each department to which an IP belongs.
+# (I figured out how to make it work with Tuples of length 1, so don't worry
+# about what I said the other day with strings)
+
+
+
+
+            
+            
+                
+            
