71d7ca1bf48edc219cbe9f9e79c88504c2982e26
Charles M <cmanker@email.unc.edu>
2013-12-08 14:55:56 -0500

    Updated to demo specifications

--- Department.py
+++ Department.py
@@ -27,16 +27,15 @@ class Department:
 		if len(ninetyDays) > 0: 
 			file.write("The following vulnerabilities have been on your system for a period longer than 90 days and are in gross violation of UNC's security policies. You will be contacted by ITS Security.\n\n")
 			for vuln in ninetyDays:
-				file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()" Level:"+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
+				file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()+" Level: "+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
 		if len(sixtyDays) > 0:
 			file.write("The following vulnerabilities have been on your system for a period greater than 60 days. Please address them as soon as possible or immediate action will be taken by the university.\n\n")
 			for vuln in sixtyDays:
-				file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()" Level:"+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
+				file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()+" Level: "+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
 		if len(thirtyDays) > 0:
 			file.write("The following vulnerabilities have been on your system for a period greater than 30 days and are no longer in compliance with UNC standards. Please address them as soon as possible.\n\n")
 			for vuln in thirtyDays:
-				file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()" Level:"+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
-
+				file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()+" Level: "+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
 		file.close()
 		if ((len(sixtyDays) != 0) or (len(thirtyDays) != 0) or (len(ninetyDays) != 0)) and (self.emailflag == 1):
 			self.writeMail()  
@@ -47,7 +46,7 @@ class Department:
 		import smtplib
 		commaspace = ', '
 		sending = self.emails 
-		sending.append('security@unc.edu') #adds security to list of emails to be sent to
+		sending.append('cmanker@email.unc.edu') #adds security to list of emails to be sent to
 		tolist = commaspace.join(sending) # puts it in the proper format for email header
 		from email.MIMEText import MIMEText 
 		text = open("./"+self.path+"/"+self.name+".txt",'r')
@@ -61,16 +60,16 @@ class Department:
 		smtp.quit()
 	def referral(self,ninetyDays): # This is called in the case where you have >90 days vulns
 		import smtplib
-		from email.mime.text import MIMEText
+		from email.MIMEText import MIMEText
 		text = "The department "+self.name+" has had the following vulnerabilities on its system for a period of greater than 90 days.\n\n"
 		for vuln in ninetyDays:
-			text = text + str(vuln[2]) + " days "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()" Level:"+vuln[0].getLevel()+" Description: "+vuln[0].getSum_threat()+"\n\n"
+			file.write(str(vuln[2])+" days - "+vuln[1]+" QID: "+vuln[0].getQID()+" "+vuln[0].getName()+" Level: "+vuln[0].getLevel()+" Threat: "+vuln[0].getSum_threat()+" Impact: "+vuln[0].getSum_impact()+"\n\n")
 		msg = MIMEText(text)
 		msg['Subject'] = "VPR_Referral "+self.name
-		msg['To'] = 'security@unc.edu' 
+		msg['To'] = 'cmanker@email.unc.edu' 
 		msg['From'] = 'security@unc.edu' 
 		smtp = smtplib.SMTP('relay.unc.edu',25)
-		smtp.sendmail('security@unc.edu',['security@unc.edu'],msg.as_string())
+		smtp.sendmail('security@unc.edu',['cmanker@email.unc.edu'],msg.as_string())
 		smtp.quit()
 	def addServer(self, server):
 		self.servers.append(server)
